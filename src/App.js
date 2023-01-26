2fc812c6de57f7afb3a4d972023f2db391f5424f
marta cesar gonzalez <martacesarg@gmail.com>
2023-01-26 18:30:20 +0100

    1.10 Exercises

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 
 import { useState } from 'react'
 
+const Button = (props) => {
+
+return (
+      <button onClick={() => props.setCounter(props.counter+ 1)}>{props.text}</button>
+      
+)
+
+}
+
+
+
+const StatisticLine = (props) => {
+
+return (
+      
+      <p>{props.text} {props.value}</p>
+      // <p>neutral: {props.neutral}</p>
+      // <p>bad: {props.bad}</p>
+)
+
+
+}
+
 const Statistics = (props) => {
+
+  if (props.good + props.neutral + props.bad===0) {
+    return (
+
+      <h3>
+      No feedback given
+      </h3>)
+    }
 
   return(
     <div>
       <h2>statistics</h2>
-      <p>good: {props.good}</p>
-      <p>neutral: {props.neutral}</p>
-      <p>bad: {props.bad}</p>
-      <p>all:{props.good + props.neutral + props.bad}</p>
-      <p>average:{(props.good * 1 + props.bad * -1)/(props.good + props.neutral + props.bad)}</p>
-      <p>positive:{100 * props.good/(props.good + props.neutral + props.bad)}</p>
-    </div>
+
+      <StatisticLine text="good" value ={props.good} />
+      <StatisticLine text="neutral" value ={props.neutral} />
+      <StatisticLine text="bad" value ={props.bad} />
+      <StatisticLine text="all" value ={props.good + props.neutral + props.bad} />
+      <StatisticLine text="average" value ={(props.good * 1 + props.bad * -1)/(props.good + props.neutral + props.bad)} />
+      <StatisticLine text="positive" value ={100 * props.good/(props.good + props.neutral + props.bad)} />
+
+
+
+      
+      </div>
   )
 
 }
@@ -27,9 +63,13 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+     <Button counter = {good} setCounter = {setGood} text="good"/>
+     <Button counter = {neutral} setCounter = {setNeutral} text="neutral"/>
+     <Button counter = {bad} setCounter = {setBad} text="bad"/>
+
+
+
+
 
       <Statistics good = {good}
                   bad = {bad}
