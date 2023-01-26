cdcd5b500ec6bfe5c0abc49c7a04cb296fb93c62
marta cesar gonzalez <martacesarg@gmail.com>
2023-01-26 18:45:55 +0100

    1.11 Exercise

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ return (
 const StatisticLine = (props) => {
 
 return (
-      
-      <p>{props.text} {props.value}</p>
-      // <p>neutral: {props.neutral}</p>
-      // <p>bad: {props.bad}</p>
+      <tr>
+      <td>{props.text}</td>
+      <td> {props.value}</td>
+      </tr>
 )
 
 
@@ -37,14 +37,16 @@ const Statistics = (props) => {
   return(
     <div>
       <h2>statistics</h2>
-
+      <table>
+      <tbody>
       <StatisticLine text="good" value ={props.good} />
       <StatisticLine text="neutral" value ={props.neutral} />
       <StatisticLine text="bad" value ={props.bad} />
       <StatisticLine text="all" value ={props.good + props.neutral + props.bad} />
       <StatisticLine text="average" value ={(props.good * 1 + props.bad * -1)/(props.good + props.neutral + props.bad)} />
       <StatisticLine text="positive" value ={100 * props.good/(props.good + props.neutral + props.bad)} />
-
+      </tbody>
+      </table>
 
 
       
